a73cd0858776632693a43b69aa8a1a23b4cbcd6d
Lasitha Siriweera <lasitha.siriweera@gmail.com>
2019-02-28 10:16:06 +0530

    SignIn component

---
+++ Components/SignIn.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, View, Text} from 'react-native';
+
+
+export default class SignIn extends React.Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    SignIn
+                </Text>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
